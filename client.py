eb0d0f173f49bd22c502c427d61ea2e6c58cb507
Gabe Fierro <gtfierro225@gmail.com>
2015-02-06 14:37:56 -0800

    change client implementation for new protocol

--- client.py
+++ client.py
@@ -1,72 +1,76 @@
 import asyncore
 import msgpack
-from collections import deque
-import random
 import socket
 import time
+import random
+from io import BytesIO
 
 sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
 ip, port = '::', 7000
 nodeid = 0xbeef
 msg = {'nodeid': nodeid,
-	   'oper': None,
-	   'echo': None,
-	   'data': None}
+       'oper': None,
+       'echo': None,
+       'data': None}
 
 def insert(data, echo):
-	msg['data'] = data
-	msg['echo'] = echo
-	msg['oper'] = 'INSERT'
-	return msgpack.packb(msg)
-
-sendqueue = deque()
+    msg['data'] = data
+    msg['echo'] = echo
+    msg['oper'] = 'INSERT'
+    return msgpack.packb(msg)
 
 class MPDBClient(asyncore.dispatcher):
-	def __init__(self):
-		self.i = 1
-		asyncore.dispatcher.__init__(self)
-		self.create_socket(socket.AF_INET6, socket.SOCK_DGRAM)
-		self.bind(('::',0))
-		self.unacked = {}
-	
-	def handle_read(self):
-		data= self.recv(2048)
-		unp = msgpack.unpackb(data)
-		if random.randint(1,10) not in [1,2,3]:
-			print 'RECEIVED', unp
-			self.unacked.pop(unp['echo'])
-		else:
-			print 'DROP RECV',unp['echo']
-	
-	def writable(self):
-		time.sleep(.5)
-		return True
-	
-	def sendmaybe(self, msg):
-		if random.randint(1,10) not in range(1,4):
-			self.sendto(msg, (ip, port))
-		else:
-			print 'DROP'
-	
-	def handle_write(self):
-		if len(self.unacked):
-			for echo, struct in self.unacked.iteritems():
-				print 'write',echo
-				if time.time() - struct['sent'] > 10:
-					print "TIMEOUT", echo
-					self.unacked.pop(echo)
-					continue # 10 second timeout on retry
-				self.sendmaybe(struct['msg'])#, (ip, port))
-			return
-		else:
-			msg = insert({"a": 1}, self.i)
-			self.unacked[self.i] = {'msg': msg, 'sent': time.time()}
-			if random.randint(1,10) not in [1,2,3]:
-				print 'write',self.i
-				self.sendmaybe(msg)#, (ip, port))
-			else:
-				print 'DROP WRITE',self.i
-		self.i += 1
+    def __init__(self):
+        self.i = 1
+        asyncore.dispatcher.__init__(self)
+        self.create_socket(socket.AF_INET6, socket.SOCK_DGRAM)
+        self.bind(('::',0))
+        self.window = {}
+        self.windowStart = 1
+        self.inair = 0
 
-client = MPDBClient()
+    def handle_read(self):
+        data= self.recv(2048)
+        buf = BytesIO(data)
+        unpacker = msgpack.Unpacker(buf)
+        for unp in unpacker:
+            if random.randint(1,10) not in []:
+                print 'RECEIVED', unp
+                if unp['echo'] in self.window:
+                    self.inair -= 1
+                    self.window.pop(unp['echo'])
+                    self.windowStart += 1
+            else:
+                print 'DROP RECV',unp['echo']
+    
+    def writable(self):
+        time.sleep(2)
+        return True
+    
+    def sendmaybe(self, msg):
+        if random.randint(1,10) not in []:
+            self.sendto(msg, (ip, port))
+        else:
+            print 'DROP'
+
+    def handle_write(self):
+        if len(self.window) == 5:
+            # do resend
+            for echo, msg in self.window.iteritems():
+                print 'resending',echo
+                time.sleep(2)
+                self.sendmaybe(msg['msg'])
+            return
+        else:
+            # send next one
+            msg = insert({str(self.i): self.i}, self.i)
+            self.window[self.i] = {'msg': msg, 'sent': time.time()}
+            print 'write',self.i
+            self.sendmaybe(msg)
+            self.i += 1
+
+
+        
+
+client= MPDBClient()
 asyncore.loop()
